b734d91267fe83b026563c07666668bd288417a4
Simon Brenner <olsner@gmail.com>
2011-01-18 20:50:39 +0100

    Add missing bits and print bit numbers in rflags tool.

--- rflags.c
+++ rflags.c
@@ -7,7 +7,7 @@ int main()
 	__asm__ __volatile__ ("pushf; popq %0" : "=g" (rflags) ::"memory");
 	printf("rFLAGS: %016llx\n", (unsigned long long)rflags);
 #define BIT(name, number) \
-	printf("\t%s: %d\n", name, rflags & (1 << number) ? 1 : 0)
+	printf("#%d\t%s:\t%d\n", number, name, rflags & (1 << number) ? 1 : 0)
 
 	const char* bits[] = {
 		"CF",
@@ -24,7 +24,14 @@ int main()
 		"OF (Overflow)",
 		"IOPL LSB",
 		"IOPL MSB",
-		"NT"
+		"NT",
+		"Reserved (0)",
+		"RF (Resume)",
+		"VM",
+		"AC (Alignment check)",
+		"VIF",
+		"VIP",
+		"ID"
 	};
 	int i;
 	for (i=0;i<sizeof(bits)/sizeof(*bits);i++)
