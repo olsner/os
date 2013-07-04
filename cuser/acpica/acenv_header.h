86ec5c0730c9500307e7f6a905a26d3ebf199182
Simon Brenner <olsner@gmail.com>
2013-07-05 00:27:50 +0200

    acpica: Make it possible to disable most of the debugging

--- cuser/acpica/acenv_header.h
+++ cuser/acpica/acenv_header.h
@@ -1,27 +1,25 @@
 #define ACPI_MACHINE_WIDTH 64
 #define ACPI_SINGLE_THREADED
-#define ACPI_DEBUG_OUTPUT
-#define ACPI_DISASSEMBLER
 #define ACPI_USE_LOCAL_CACHE
 #define ACPI_INLINE inline
 #define ACPI_USE_NATIVE_DIVIDE
 
+#define DEBUGGER_THREADING DEBUGGER_SINGLE_THREADED
+
+#ifdef ACPI_FULL_DEBUG
+#define ACPI_DEBUG_OUTPUT
+#define ACPI_DISASSEMBLER
+
 // Depends on threading support
 #define ACPI_DEBUGGER
-#define DEBUGGER_THREADING DEBUGGER_SINGLE_THREADED
-// Depends on debugger support
-//#define ACPI_DBG_TRACK_ALLOCATIONS
-
-#define ACPI_PHYS_BASE 0x100000000
-
-#if 0
-#define AcpiException(ModName, Line, Status, Format, ...) \
-	AcpiOsPrintf(ACPI_MSG_EXCEPTION "%s, " Format, \
-		AcpiFormatException(Status), \
-		## __VA_ARGS__)
-#endif
+#define ACPI_DBG_TRACK_ALLOCATIONS
 
 #define ACPI_GET_FUNCTION_NAME __FUNCTION__
+#else
+#define ACPI_GET_FUNCTION_NAME ""
+#endif
+
+#define ACPI_PHYS_BASE 0x100000000
 
 #include <stdint.h>
 #include <stdarg.h>
