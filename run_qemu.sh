1d2608046edef79d9a23686a2e6fa36d61885332
Simon Brenner <olsner@gmail.com>
2013-07-10 20:09:45 +0200

    run_qemu.sh: Set up tap0 networking

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,2 +1,2 @@
-make out/grub.iso &&
-kvm -m 32M "$@" -cdrom out/grub.iso -net nic,model=e1000 -net user
+make -j4 out/grub.iso &&
+kvm -m 32M "$@" -cdrom out/grub.iso -netdev tap,id=tap0,script=no,ifname=tap0,downscript=no -device e1000,netdev=tap0
