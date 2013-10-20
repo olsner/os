d20c1f7e92dd551d0d0c2c9eed2e8542145bfddb
Simon Brenner <olsner@gmail.com>
2013-10-20 15:50:21 +0200

    Build everything (not just the iso) from run_qemu.sh

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,4 +1,4 @@
 ISO=${ISO-out/grub.iso}
-make -j4 $ISO &&
+make -j4 &&
 kvm -m 32M "$@" -cdrom $ISO -netdev tap,id=vmnet0,script=no,ifname=tap0,downscript=no -device e1000,netdev=vmnet0
 #kvm -m 32M "$@" -cdrom $ISO -netdev user,id=vmnet0 -device e1000,netdev=vmnet0
