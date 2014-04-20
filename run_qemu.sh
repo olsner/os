65b41ddb01f4f70cb5ea8c84258eece6a21cfb5f
Simon Brenner <olsner@gmail.com>
2014-04-20 21:03:42 +0200

    Make it easier to switch netdev config in run_qemu.sh

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,4 +1,5 @@
 ISO=${ISO-out/grub.iso}
 make -j4 &&
-kvm -m 32M "$@" -cdrom $ISO -netdev tap,id=vmnet0,script=no,ifname=tap0,downscript=no -device e1000,netdev=vmnet0
-#kvm -m 32M "$@" -cdrom $ISO -netdev user,id=vmnet0 -redir tcp:5555::80 -device e1000,netdev=vmnet0
+#NETDEV=${NETDEV-tap,id=vmnet0,script=no,ifname=tap0,downscript=no}
+NETDEV="user,id=vmnet0 -redir tcp:5555::80"
+kvm -m 32M "$@" -cdrom $ISO -netdev $NETDEV -device e1000,netdev=vmnet0
