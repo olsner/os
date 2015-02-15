11b05d162255ef626f4ef6926f54e65c366ea124
Simon Brenner <olsner@gmail.com>
2015-02-15 21:16:09 +0100

    Add an xhci and a USB mouse to qemu configuration

--- run_qemu.sh
+++ run_qemu.sh
@@ -3,4 +3,5 @@ make -j4 || exit $?
 VGA="-vga std"
 NETDEV="user,id=vmnet0 -redir tcp:5555::80"
 #NETDEV=${NETDEV-tap,id=vmnet0,script=no,ifname=tap0,downscript=no}
-${KVM-kvm} -m 32M $VGA "$@" -cdrom $ISO -netdev $NETDEV -device e1000,netdev=vmnet0
+USB="-usb -device nec-usb-xhci,id=xhci -usbdevice mouse"
+${KVM-kvm} -m 32M $VGA "$@" -cdrom $ISO -netdev $NETDEV -device e1000,netdev=vmnet0 $USB
