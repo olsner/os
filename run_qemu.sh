eb06d268803a96cff1d31e5b114030616d9e7da9
Simon Brenner <olsner@gmail.com>
2016-05-07 17:22:30 +0200

    Add another CPU to qemu run

--- run_qemu.sh
+++ run_qemu.sh
@@ -3,4 +3,6 @@ make -j4 || exit $?
 VGA="-vga std"
 NETDEV="user,id=vmnet0 -redir tcp:5555::80"
 #NETDEV=${NETDEV-tap,id=vmnet0,script=no,ifname=tap0,downscript=no}
-${KVM-kvm} -m 32M $VGA "$@" -cdrom $ISO -netdev $NETDEV -device e1000,netdev=vmnet0
+# New syntax required if using -machine q35
+#CD="-drive file=$ISO,if=none,media=cdrom,id=cd1 -device ide-cd,drive=cd1"
+${KVM-kvm} -smp 2 -m 32M $VGA "$@" -cdrom $ISO -netdev $NETDEV -device e1000,netdev=vmnet0
