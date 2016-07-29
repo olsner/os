09c0d02fef478d847efde760c99f2bd59ef19896
Simon Brenner <olsner@gmail.com>
2016-07-29 15:27:01 +0200

    kcpp: Switch to C++14 standard

--- kcpp/main.cc
+++ kcpp/main.cc
@@ -71,10 +71,16 @@ void *operator new(size_t sz) {
 void operator delete(void *p) {
     free(p);
 }
+void operator delete(void *p, size_t) {
+    free(p);
+}
 void *operator new[](size_t sz) {
     return malloc(sz);
 }
 void operator delete[](void *p) {
+    free(p);
+}
+void operator delete[](void *p, size_t) {
     free(p);
 }
 
