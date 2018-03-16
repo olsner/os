73a51ceb241f9fc99368b5a0cd3a801d26f16a8f
Simon Brenner <simon.brenner@vaderstad.com>
2018-03-16 16:04:14 +0100

    Use qemu-system-x86_64 by default instead of "kvm"
    IIRC they're actually the same on Linux, and the "kvm" alias is of course
    not installed on Mac.

--- run_qemu.sh
+++ run_qemu.sh
@@ -5,4 +5,4 @@ NETDEV="user,id=vmnet0 -redir tcp:5555::80"
 #NETDEV=${NETDEV-tap,id=vmnet0,script=no,ifname=tap0,downscript=no}
 # New syntax required if using -machine q35
 #CD="-drive file=$ISO,if=none,media=cdrom,id=cd1 -device ide-cd,drive=cd1"
-${KVM-kvm} -cpu SandyBridge -smp 2 -m 32M $VGA "$@" -cdrom $ISO -netdev $NETDEV -device e1000,netdev=vmnet0
+${QEMU-qemu-system-x86_64} -cpu SandyBridge -smp 2 -m 32M $VGA "$@" -cdrom $ISO -netdev $NETDEV -device e1000,netdev=vmnet0
