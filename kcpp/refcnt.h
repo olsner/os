5f9b053e12bcf40a8b807dc95ec3719aea15840f
Simon Brenner <olsner@gmail.com>
2020-07-14 16:49:58 +0200

    Add some more C++ness, constructors and operators to RefCnt

--- kcpp/refcnt.h
+++ kcpp/refcnt.h
@@ -1,70 +1,99 @@
-template <typename T>
-class RefCnt
+#pragma once
+
+// Intrusive shared_ptr, should be replaced with the actual shared_ptr I think.
+template <typename T> class RefCnt
 {
     T *ptr_;
+
+    void addref() {
+        if (ptr_) ptr_->addref();
+    }
+
+    template <typename U> friend class RefCnt;
 public:
     explicit RefCnt(T* ptr): ptr_(ptr) {
-        if (ptr) ptr->addref();
+        addref();
     }
     ~RefCnt() {
         if (ptr_) ptr_->release();
     }
 
     RefCnt(): ptr_(nullptr) {}
-    explicit RefCnt(nullptr_t): ptr_(nullptr) {}
-    RefCnt(RefCnt&& other) {
+    RefCnt(nullptr_t): ptr_(nullptr) {}
+    RefCnt(const RefCnt& other) {
         ptr_ = other.ptr_;
-        other.ptr_ = nullptr;
+        addref();
+    }
+    template <typename U> RefCnt(const RefCnt<U>& other) {
+        ptr_ = other.ptr_;
+        addref();
+    }
+    RefCnt(RefCnt&& other): ptr_(latch(other.ptr_, nullptr)) {
+    }
+    template <typename U> RefCnt(RefCnt<U>&& other): ptr_(latch(other.ptr_, nullptr)) {
     }
     void operator=(RefCnt&& other) {
-        ptr_ = other.ptr_;
-        other.ptr_ = nullptr;
+        ptr_ = latch(other.ptr_, nullptr);
     }
     void operator=(const RefCnt& other) {
         ptr_ = other.ptr_;
-        if (ptr_) ptr_->addref();
+        addref();
+    }
+    template <typename U> void operator=(RefCnt<U>&& other) {
+        ptr_ = latch(other.ptr_, nullptr);
     }
 
     void reset(nullptr_t p = nullptr) {
         if (ptr_) ptr_->release();
         ptr_ = nullptr;
     }
-    void reset_addref(T* ptr) {
-        if (ptr_) ptr_->release();
-        ptr_ = ptr;
-        if (ptr_) ptr_->addref();
+
+    // The "const T" returning variants should be accessible through
+    // RefCnt<const T> (or at least that's how shared_ptr works).
+    T* operator->() const {
+        return ptr_;
     }
-    void reset_take(T* ptr) {
-        if (ptr_) ptr_->release();
-        ptr_ = ptr;
+    T* get() const {
+        return ptr_;
     }
 
-    T* operator->() {
-        return ptr_;
+    bool operator==(const RefCnt& other) const {
+        return ptr_ == other.ptr_;
     }
-    const T* operator ->() const {
-        return ptr_;
+    template <typename U> bool operator==(const RefCnt<U>& other) const {
+        return ptr_ == other.ptr_;
     }
-    T* get() {
-        return ptr_;
+    template <typename U> bool operator==(const U* rawptr) const {
+        return ptr_ == rawptr;
+    }
+    bool operator==(nullptr_t) const {
+        return ptr_ == nullptr;
+    }
+    bool operator!=(nullptr_t) const {
+        return ptr_ != nullptr;
+    }
+    operator bool() const {
+        return ptr_ != nullptr;
     }
 };
+
+template <typename T, typename... Args>
+RefCnt<T> make_refcnt(Args&&... args) {
+    return RefCnt(new T(static_cast<Args>(args)...));
+}
 
 template <typename T>
 class RefCounted
 {
-    uint32_t count;
+    uint32_t count = 0;
 
     void deleteme() {
         delete static_cast<T*>(this);
     }
-public:
-    RefCounted(): count(0) {}
-    RefCounted(const RefCounted&) = delete;
-    RefCounted& operator =(const RefCounted&) = delete;
+
+    template <typename U> friend class RefCnt;
 
     void addref() {
-        assert(count < UINT32_MAX);
         count++;
     }
     void release() {
@@ -72,6 +101,11 @@ public:
         if (!--count)
             deleteme();
     }
+
+public:
+    RefCounted() = default;
+    RefCounted(const RefCounted&): RefCounted() {}
+    RefCounted& operator =(const RefCounted&) { return this; }
 
     uint32_t get_refcount() const {
         return count;
